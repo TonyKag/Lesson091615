f133ce25c5264487fbdfaa987829141cf74fb67f
TonyKag <tonykag@mail.com>
2015-09-16 19:18:54 -0700

    new full comments were added

--- Calculate.java
+++ Calculate.java
@@ -2,12 +2,23 @@
 // adding arguments, separated by spaces
 // version 1.0
 
-public class Calculate {					// init class
- public static void main(String[] args) {	// create main method
-  int total = 0;							// init result variable and init as 0
-  for(int i = 0; i < args.length; i++) {	// loop for calculating all arguments
-   total += Integer.parseInt(args[i]);		// parse each argument into int type
+// Can only have one public class
+// This class name needs to match the file name Calculate.java
+// Creates a file with same name that ends in *.class
+public class Calculate {
+ // Core method for an application, must match the signature exactly
+ public static void main(String[] arg) {
+  // Set a variable of type int named total to 0
+  int total = 0;
+  // Iterate through a loop, 
+   // Init: Set int type variable i = 0
+   // Condition: As long as i is less than the total length of args array
+   // Increment: increas i by 1 for each iteration
+  for(int i = 0; i < arg.length; i++) {
+   // add a value to the total for each element in the args array
+   total += Integer.parseInt(arg[i]);
   } 
-  System.out.println("The total is " + total); // Output result
+  // Print out to the console the total of all the elements combined
+  System.out.println("The total is " + total);
  }
 }
