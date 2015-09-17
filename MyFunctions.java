b1875dd733bdfdb4a792e949696f5233a0e05364
TonyKag <tonykag@mail.com>
2015-09-16 20:42:28 -0700

    MyFunction is added

---
+++ MyFunctions.java
@@ -0,0 +1,20 @@
+// Program that has a function to print out whether the number 
+// is greater than 10
+public class MyFunctions {
+	// Core method for an application, must match the signature exactly
+	public static void main(String[] args) {
+		// Create a variable and set it to first passed argument
+		int passedValue = Integer.parseInt(args[0]);
+
+		// Code to check if the number is greater than 10
+		if( passedValue > 10) {
+			System.out.println("The number " + passedValue + " is greater than 10");
+		}
+		else if( passedValue == 10) {
+			System.out.println("The number " + passedValue + " is equal 10");
+		}
+		else if( passedValue < 10) {
+			System.out.println("The number " + passedValue + " is less than 10");
+		}
+	}
+}
