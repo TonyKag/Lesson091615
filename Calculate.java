5a3d69842179e61bd0dda3fccb51c85ac20417f3
TonyKag <tonykag@mail.com>
2015-09-16 18:49:43 -0700

    comments for each line were added

--- Calculate.java
+++ Calculate.java
@@ -2,12 +2,12 @@
 // adding arguments, separated by spaces
 // version 1.0
 
-public class Calculate {
- public static void main(String[] args) {
-  int total = 0;
-  for(int i = 0; i < args.length; i++) {
-   total += Integer.parseInt(args[i]);
+public class Calculate {					// init class
+ public static void main(String[] args) {	// create main method
+  int total = 0;							// init result variable and init as 0
+  for(int i = 0; i < args.length; i++) {	// loop for calculating all arguments
+   total += Integer.parseInt(args[i]);		// parse each argument into int type
   } 
-  System.out.println("The total is " + total);
+  System.out.println("The total is " + total); // Output result
  }
 }
